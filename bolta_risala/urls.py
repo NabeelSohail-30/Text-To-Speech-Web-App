5bad9cb4ca0d748476271683ce7bcdff72217739
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2024-04-17 07:51:30 +0500

    Add generate_speech_eleven endpoint to urls.py

--- bolta_risala/urls.py
+++ bolta_risala/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from speech_generation.views import converter_view, generate_speech, display_speeches, serve_audio_file
+from speech_generation.views import converter_view, generate_speech, display_speeches, serve_audio_file, generate_speech_eleven
 from user_auth.views import custom_logout_view, custom_logout_page
 from data_dictionary.views import data_dictionary
 
@@ -30,6 +30,8 @@ urlpatterns = [
     path('logout_page/', custom_logout_page, name='custom_logout_page'),
     path('converter/', converter_view, name='converter'),
     path('generate_speech', generate_speech, name='generate_speech'),
+    path('generate_speech_eleven', generate_speech_eleven,
+         name='generate_speech_eleven'),
     path('speeches', display_speeches, name='display_speeches'),
     path('data_dictionary', data_dictionary, name='data_dictionary'),
     path('<path:path>', serve_audio_file, name='speech_file'),
