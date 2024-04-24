be05dd294a5a8f5fd3f0f4df61de564bc1e08ed2
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2024-04-24 20:19:25 +0500

    Update max_length of actual_word and replaced_word fields in DataEntry model

--- data_dictionary/models.py
+++ data_dictionary/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class DataEntry(models.Model):
-    actual_word = models.CharField(max_length=100)
-    replaced_word = models.CharField(max_length=100)
+    actual_word = models.CharField(max_length=1000)
+    replaced_word = models.CharField(max_length=1000)
 
     def __str__(self):
         return f"{self.actual_word} - {self.replaced_word}"
