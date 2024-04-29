44e22372066d6ec126a97cd2d4281e544172aa9c
Nabeel Sohail <nabeel.sohail2630@outlook.com>
2024-04-29 09:05:39 +0500

    reset to old code, the project is working fine removed the merged app

--- bolta_risala/settings.py
+++ bolta_risala/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'user_auth',
     'speech_generation',
     'data_dictionary',
-    'merge_audios'
 ]
 
 MIDDLEWARE = [
--- merge_audios/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
-
-def upload_audio(request):
-    return render(request, 'upload_audio.html')
